6c690296a21cf3b254516aceb4da1fc8396a6275
Antonio Piras <antonio@syntegral.co>
2023-04-04 07:19:10 +0000

    Main

---
+++ src/utils/tracking.ts
@@ -0,0 +1,23 @@
+import mixpanel from 'mixpanel-browser'
+
+const isProduction = import.meta.env.MODE === 'production'
+mixpanel.init(import.meta.env.VITE_MIXPANEL_PROJECT_TOKEN, { debug: isProduction })
+
+const Mixpanel = {
+  identify: (id: string) => {
+    if (!isProduction) return
+    mixpanel.identify(id)
+  },
+  track: (name: string, props: object) => {
+    if (!isProduction) return
+    mixpanel.track(name, props)
+  },
+  people: {
+    set: (props: object) => {
+      if (!isProduction) return
+      mixpanel.people.set(props)
+    },
+  },
+} as const
+
+export default Mixpanel
