0eb76b723d4c951799e664569cd9e3ffd0f8ecf9
Antonio Piras <antopiras89@gmail.com>
2023-08-18 11:39:33 +0200

    Add more tests and correct reference types

--- src/utils/helpers.test.ts
+++ src/utils/helpers.test.ts
@@ -1,10 +1,12 @@
 import { describe, expect, test } from 'vitest'
-import { capitalize, formatReferences } from './helpers'
+import { capitalize, formatReferences, getThemeScore } from './helpers'
 
 describe('Helpers', () => {
-  test('Capitalize returns a capialized string', () => {
-    const lowercaseString = 'lorem ipsum'
-    expect(capitalize(lowercaseString)).toBe('Lorem ipsum')
+  describe('String formatting helpers', () => {
+    test('Capitalize returns a capialized string', () => {
+      const lowercaseString = 'lorem ipsum'
+      expect(capitalize(lowercaseString)).toBe('Lorem ipsum')
+    })
   })
 
   describe('References formatting helpers', () => {
@@ -17,6 +19,30 @@ describe('Helpers', () => {
       expect(formatReferences(references)).toStrictEqual([
         { id: '1', page: '2', name: 'Lorem ipsum' },
       ])
+    })
+  })
+
+  describe('Themes helpers', () => {
+    const themeId = 2
+    const themeScores = [
+      {
+        assetId: 1,
+        themeId: 1,
+        score: 90,
+      },
+      {
+        assetId: 1,
+        themeId: 2,
+        score: 100,
+      },
+    ]
+
+    test('Return score of zero for non existing score', () => {
+      expect(getThemeScore(3, themeScores)).toEqual(0)
+    })
+
+    test('Return correct theme score', () => {
+      expect(getThemeScore(themeId, themeScores)).toEqual(100)
     })
   })
 })
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,6 @@
+/* c8 ignore next 7 */
 import { DocumentLink } from '../components/document-modal/types'
+import { ThemeScore } from '../pages/assets/types'
 
 export function assertUnreachable(x: never): never {
   throw new Error('Unreachable code!')
@@ -9,7 +11,7 @@ export function capitalize(string: string) {
 }
 
 export function formatReferences(
-  references: string[][] | string[] | undefined,
+  references: string[][] | undefined,
 ): DocumentLink[] {
   if (!references) return []
 
@@ -20,17 +22,7 @@ export function formatReferences(
   }))
 }
 
-export function formatQAReferences(references: string[]): DocumentLink[] {
-  if (!references) return []
-
-  return references.map((reference) => ({
-    id: reference[0],
-    page: reference[1],
-    name: reference[2],
-  }))
-}
-
-export function getThemeScore(themeId: number, scores: any[]) {
+export function getThemeScore(themeId: number, scores: ThemeScore[]) {
   const themeScore = scores.find((score) => score.themeId === themeId)
 
   if (!themeScore) return 0
@@ -38,6 +30,7 @@ export function getThemeScore(themeId: number, scores: any[]) {
   return themeScore.score
 }
 
+/* c8 ignore next 3 */
 export function delay(ms: number) {
   return new Promise((res) => setTimeout(res, ms))
 }
