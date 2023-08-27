3106e1e0f5142b812bbeb979c305e3311e578df0
Antonio Piras <antopiras89@gmail.com>
2023-08-27 16:53:00 +0200

    Enable Sentry logging for production only

--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -9,19 +9,21 @@ import * as Sentry from '@sentry/react'
 
 const isProduction = import.meta.env.MODE === 'production'
 
-Sentry.init({
-  dsn: 'https://3e0017611a2e469e9938a8a2af409c99@o4505498617708544.ingest.sentry.io/4505498620592128',
-  release: '1.0.0',
-  integrations: [
-    new Sentry.BrowserTracing({
-      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
-        useEffect,
-        useLocation,
-        useNavigationType,
-        createRoutesFromChildren,
-        matchRoutes,
-      ),
-    }),
-  ],
-  tracesSampleRate: isProduction ? 0.5 : 1.0, // Capture 100% of the transactions, reduce in production!
-})
+if (isProduction) {
+  Sentry.init({
+    dsn: 'https://3e0017611a2e469e9938a8a2af409c99@o4505498617708544.ingest.sentry.io/4505498620592128',
+    release: '1.0.0',
+    integrations: [
+      new Sentry.BrowserTracing({
+        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+          useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes,
+        ),
+      }),
+    ],
+    tracesSampleRate: 0.5,
+  })
+}
