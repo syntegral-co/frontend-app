fff5a4134ae19a17a5b3b6863341b69a85d27f32
Antonio Piras <antopiras89@gmail.com>
2023-09-07 18:31:59 +0200

    Load modal before downloading PDF

--- src/components/document-modal/index.tsx
+++ src/components/document-modal/index.tsx
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom'
 import Loader from '../loader'
 
 function DocumentModal() {
-  const { documentPage, documentUrl, status, fetchStatus } = useDocumentModal()
+  const { documentId, documentPage, documentUrl, status, fetchStatus } =
+    useDocumentModal()
 
-  if (!documentUrl || !documentPage) return <></>
+  if (!documentId || !documentPage) return <></>
 
   return (
     <div className="fixed left-1/2 top-1/2 z-10 m-auto h-5/6 w-5/6 -translate-y-1/2 -translate-x-1/2 rounded-md border-2 border-accent bg-base-100 shadow-lg">
@@ -14,14 +15,16 @@ function DocumentModal() {
         {status === 'success' && fetchStatus === 'idle' && (
           <object
             className="mb-4"
-            data={`${Object.values(documentUrl)[0]}#page=${documentPage}`}
+            data={`${Object.values(documentUrl!)[0]}#page=${documentPage}`}
             type="application/pdf"
             width="100%"
             height="100%"
           >
             <p>
               Link{' '}
-              <a href={`${Object.values(documentUrl)[0]}#page=${documentPage}`}>
+              <a
+                href={`${Object.values(documentUrl!)[0]}#page=${documentPage}`}
+              >
                 to the PDF!
               </a>
             </p>
