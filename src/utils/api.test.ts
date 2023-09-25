b4f63894e8529c03f17ae77e4b90163316b5e1c9
Antonio Piras <antopiras89@gmail.com>
2023-09-25 15:38:29 +0200

    Add more tests for API caller and session manager

---
+++ src/utils/api.test.ts
@@ -0,0 +1,22 @@
+import { describe, expect, test, vi } from 'vitest'
+import { callAPI } from './api'
+
+describe('callAPI', () => {
+  test('callAPI returns a valid response', async () => {
+    const result = await vi.waitFor(async () => {
+      const response = await callAPI(
+        'https://jsonplaceholder.typicode.com/todos/1',
+      )
+
+      return response
+    })
+
+    expect(result).not.toBe(null)
+  })
+
+  test('callAPI throws a generic error', async () => {
+    await expect(callAPI('THISDOESNOTEXIST')).rejects.toThrow(
+      'There was an error',
+    )
+  })
+})
